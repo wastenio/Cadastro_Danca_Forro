37f03b8af26b3d88f975306fb4418031ca8ac82e
wastenio da silva rocha <wastenio.silva@gmail.com>
2025-11-13 15:42:31 -0300

    Rota participantes

--- participants/urls.py
+++ participants/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 
 app_name = 'participants'
@@ -9,4 +10,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('checkin/<uuid:uuid>/', views.checkin_by_uuid, name='checkin'),
     path('validate/<uuid:uuid>/', views.validate_qr, name='validate'),
+    
+    path('', RedirectView.as_view(url='register/', permanent=False)),
 ]
