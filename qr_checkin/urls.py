ae571916f6ff030ce9efc75bcde8041eac1ee63c
wastenio da silva rocha <wastenio.silva@gmail.com>
2025-11-13 15:37:05 -0300

    Configurando rota principal

--- qr_checkin/urls.py
+++ qr_checkin/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('participants/', include('participants.urls', namespace='participants')),
+    path('', RedirectView.as_view(url='/participants/', permanent=False)),
 ]
 
 
